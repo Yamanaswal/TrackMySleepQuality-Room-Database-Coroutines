53401942eff9cfce7856ff2097d4b060db941933
Yamanaswal <yamanaswal@gmail.com>
2022-01-16 14:35:56 +0530

    Completed Room and Coroutines

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -7,30 +7,30 @@ import androidx.room.Query
 import androidx.room.Update
 
 /**
-    - Insert new nights.
-    - Update an existing night to update an end time and a quality rating.
-    - Get a specific night based on its key.
-    - Get all nights, so you can display them.
-    - Get the most recent night.
-    - Delete all entries in the database.
+- Insert new nights.
+- Update an existing night to update an end time and a quality rating.
+- Get a specific night based on its key.
+- Get all nights, so you can display them.
+- Get the most recent night.
+- Delete all entries in the database.
  * */
 
 @Dao
 interface SleepDatabaseDao {
     @Insert
-    fun insert(night: SleepNight)
+    suspend fun insert(night: SleepNight)
 
     @Update
-    fun updated(night: SleepNight)
+    suspend fun update(night: SleepNight)
 
     @Query(value = "SELECT * from daily_sleep_quality_table WHERE nightId = :key")
-    fun get(key: Long): SleepNight?
+    suspend fun get(key: Long): SleepNight?
 
     @Query(value = "DELETE FROM daily_sleep_quality_table ")
-    fun clear()
+    suspend fun clear()
 
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
-    fun getTonight(): SleepNight?
+    suspend fun getTonight(): SleepNight?
 
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
     fun getAllNights(): LiveData<List<SleepNight>>
